9721fadcff0736b89e51fc18086b4d419d02a3e1
Marcin Bondaruk <m.bondaruk@telemedi.com>
2021-12-06 21:54:29 +0100

    depth change

---
+++ 1_depth_change/depth.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function process() {
+	const fileStream = fs.createReadStream('./data.txt');
+	let counter = 0;
+	let prev;
+	rl = readline.createInterface({
+		input: fileStream,
+		crlfDelay: Infinity
+	});
+
+	for await (const line of rl) {
+		const curr = parseInt(line);
+
+		if (!prev) {
+			prev = curr;
+			continue;
+		}
+
+		if (prev - curr < 0) {
+			counter++;
+		}
+
+		prev = curr;
+
+	}
+
+	console.log(counter);
+}
+
+process();
